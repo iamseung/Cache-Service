e17e5e6cdaed0ee346e8177a232646a59bde5b77
iamseung <seesungseok@naver.com>
2025-05-12 22:09:47 +0900

    캐시 활성화

--- src/main/java/com/example/cacheservice/CacheServiceApplication.java
+++ src/main/java/com/example/cacheservice/CacheServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.cacheservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CacheServiceApplication {
 
